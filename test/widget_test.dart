6706083c68986918cfc13e74f201479751b3d0d9
VICTUS <sagarsahil3690@gmail.com>
2024-02-29 18:03:32 +0530

    Arrange files

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,9 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:untitled2/main.dart';
 
-import '../Presentation/Screens/main.dart';
+
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
